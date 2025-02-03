610bd975e689667492d18904b6bf4807de7406fb
Artem Markelov <artemmarkelov888@gmail.com>
2025-02-03 11:31:04 +0600

    сделал хедер и верхний банер

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import './App.css'
 import Header from "./components/header/Header.tsx";
 import Footer from "./components/footer/Footer.tsx";
+import imgFactory from "../src/assets/images/background/cascade-factory.svg"
 
 function App() {
 
   return (
 
     <div style={{display: 'flex', flexDirection: 'column', height: '100vh', margin: 0}}>
-      <div style={{borderBottom: '2px solid #6F7682'}}>
-        <div className={'main-container'} style={{marginBottom: 'auto'}}>
-          <Header/>
-        </div>
+      <div className={'main-container'} style={{marginBottom: 'auto', marginTop: 0}}>
+        <Header/>
+      </div>
+
+      <div style={{border: '1px solid blue', flex: 1}}>
+        <img style={{width: '100%'}} src={imgFactory} alt="factory"/>
       </div>
 
 
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,25 +1,39 @@
-import logo from '../../assets/images/logo/logo.png';
+import logo from '../../assets/images/logo/logo.svg';
 import './header.css'
 
 const Header = () => {
   return (
-    <div style={{display: 'flex', justifyContent: 'space-between', margin: 0}}>
-      <a href="#" style={{margin: 0}}>
-        <img style={{maxWidth: '200px'}} src={logo} alt="cascade"/>
-      </a>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      margin: 0,
+    }}>
+
       <div>
-        <ul style={{display: 'flex', gap: 25, listStyle: 'none'}}>
+        <ul style={{
+          display: 'flex',
+          gap: 20,
+          listStyle: 'none',
+          borderBottom: '1px solid #6F7682',
+          paddingRight: '5px',
+          paddingLeft: '20px',
+          fontSize: '12px',
+        }}>
           <li>
-            <a className={'link-header'} href="#">About</a>
+            <a className={'link-header'} href="#">О НАС</a>
           </li>
           <li>
-            <a className={'link-header'} href="#">Toilets</a>
+            <a className={'link-header'} href="#">ПРОДУКЦИЯ</a>
           </li>
           <li>
-            <a className={'link-header'} href="#">Shells</a>
+            <a className={'link-header'} href="#">КОНТАКТЫ</a>
           </li>
         </ul>
       </div>
+      <a href="#" style={{margin: 0}}>
+        <img style={{maxWidth: '179px'}} src={logo} alt="cascade.kg"/>
+      </a>
     </div>
   );
 };
