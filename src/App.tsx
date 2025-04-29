5ac00df5716275b4e14a6da6ea8ecaf7feb3a2d3
Artem Markelov <artemmarkelov888@gmail.com>
2025-04-29 17:01:48 +0600

    Все готова !1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import Header from "./components/header/Header.tsx";
 import Footer from "./components/footer/Footer.tsx";
 import Factory from "./components/factory/Factory.tsx";
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
       }
     };
 
-    handleResize(); // вызвать сразу при загрузке
+    handleResize();
 
     window.addEventListener('resize', handleResize);
 
